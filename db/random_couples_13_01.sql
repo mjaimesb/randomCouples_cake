7e9975490bc6228213dd13e5ea62529bb6c52b02
Manuel <majb.ve@gmail.com>
2021-01-13 14:29:57 +0100

    maj db backup

--- db/random_couples_13_01.sql
+++ db/random_couples_13_01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 18 déc. 2020 à 07:40
+-- Généré le :  mer. 13 jan. 2021 à 13:25
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  5.6.40
 
@@ -34,19 +34,31 @@ CREATE TABLE IF NOT EXISTS `couples` (
   `idFirst` int(11) DEFAULT NULL,
   `idSecond` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `couples`
 --
 
 INSERT INTO `couples` (`id`, `idFirst`, `idSecond`) VALUES
-(1, 1, 2),
-(2, 3, 4),
-(3, 1, 3),
-(4, 2, 4),
-(5, 1, 4),
-(6, 2, 3);
+(57, 5, 3),
+(58, 6, 2),
+(59, 7, 1),
+(60, 1, 6),
+(61, 2, 5),
+(62, 3, 4),
+(63, 4, 6),
+(64, 5, 3),
+(65, 1, 2),
+(66, 2, 6),
+(67, 3, 1),
+(68, 4, 5),
+(69, 5, 6),
+(70, 1, 4),
+(71, 2, 3),
+(72, 3, 6),
+(73, 4, 2),
+(74, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -56,24 +68,35 @@ INSERT INTO `couples` (`id`, `idFirst`, `idSecond`) VALUES
 
 DROP TABLE IF EXISTS `couples_rounds`;
 CREATE TABLE IF NOT EXISTS `couples_rounds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `couple_id` int(11) NOT NULL,
   `round_id` int(11) NOT NULL,
-  PRIMARY KEY (`couple_id`,`round_id`),
-  UNIQUE KEY `fk_couples_has_rounds_couples_idx` (`couple_id`),
-  KEY `fk_couples_has_rounds_rounds1` (`round_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `couples_rounds`
 --
 
-INSERT INTO `couples_rounds` (`couple_id`, `round_id`) VALUES
-(1, 1),
-(2, 1),
-(3, 2),
-(4, 2),
-(5, 3),
-(6, 3);
+INSERT INTO `couples_rounds` (`id`, `couple_id`, `round_id`) VALUES
+(62, 57, 66),
+(63, 58, 66),
+(64, 59, 66),
+(65, 60, 67),
+(66, 61, 67),
+(67, 62, 67),
+(68, 63, 68),
+(69, 64, 68),
+(70, 65, 68),
+(71, 66, 69),
+(72, 67, 69),
+(73, 68, 69),
+(74, 69, 70),
+(75, 70, 70),
+(76, 71, 70),
+(77, 72, 71),
+(78, 73, 71),
+(79, 74, 71);
 
 -- --------------------------------------------------------
 
@@ -101,19 +124,21 @@ DROP TABLE IF EXISTS `rounds`;
 CREATE TABLE IF NOT EXISTS `rounds` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(70) DEFAULT NULL,
-  `statut` int(11) DEFAULT NULL,
+  `statut` int(11) DEFAULT 0,
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `rounds`
 --
 
 INSERT INTO `rounds` (`id`, `name`, `statut`, `created`) VALUES
-(1, 'Round1', 1, '2020-12-17 00:00:00'),
-(2, 'Round2', 1, '2020-12-17 00:00:00'),
-(3, 'Round3', 0, '2020-12-17 00:00:00');
+(67, 'Round 1', 1, '2021-01-06 17:02:12'),
+(68, 'Round 2', 0, '2021-01-06 17:02:12'),
+(69, 'Round 3', 0, '2021-01-06 17:02:12'),
+(70, 'Round 4', 0, '2021-01-06 17:02:12'),
+(71, 'Round 5', 0, '2021-01-06 17:02:13');
 
 -- --------------------------------------------------------
 
@@ -144,26 +169,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` varchar(255) NOT NULL,
   `active` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `active`) VALUES
-(1, 'admin', '5221a29502545a0cca5a7c5fe013879272c467b8', 'admin', 1),
-(7, 'user', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1);
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `couples_rounds`
---
-ALTER TABLE `couples_rounds`
-  ADD CONSTRAINT `fk_couples_has_rounds_couples` FOREIGN KEY (`couple_id`) REFERENCES `couples` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_couples_has_rounds_rounds1` FOREIGN KEY (`round_id`) REFERENCES `rounds` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+(1, 'Sébastien', '5221a29502545a0cca5a7c5fe013879272c467b8', 'admin', 1),
+(2, 'Céline', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1),
+(3, 'Sophie', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1),
+(4, 'Selin', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1),
+(5, 'Thaïs', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1),
+(6, 'Manuel', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 1),
+(7, 'Romaric', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 0),
+(8, 'Vincent', 'ab8ce82b712bda3f89580626016c0e81a56aeacf', 'user1', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
